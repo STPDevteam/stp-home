5ffcbefa8f12e71b30336fe390b361f2ad7cb2f9
kingyoto02 <15326862+wytweb@users.noreply.github.com>
2024-03-18 12:50:42 +0800

    add orb-canvas

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,18 @@
 const CracoLessPlugin = require('craco-less');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      // Add a rule to handle .mjs files
+      webpackConfig.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      });
+
+      return webpackConfig;
+    },
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
--- src/pages/Home/homepage.tsx
+++ src/pages/Home/homepage.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { Box, Grid, Stack, Tab, Tabs, Typography } from "@mui/material";
 import styled, { keyframes } from "styled-components";
 import { Button, Divider } from "antd";
@@ -36,7 +37,7 @@ import LearnCreateDao from "../../assets/images/home/svg/learn-create-dao.svg";
 import LearnCreateToken from "../../assets/images/home/svg/learn-create-token.svg";
 import LearnOnChain from "../../assets/images/home/svg/learn-on-chian.svg";
 import ArrowUpRight from "../../assets/images/home/svg/arrow-up-right.svg";
-import React, { useMemo, useState,useRef } from "react";
+import React, { useMemo, useState,useRef, useEffect } from "react";
 import { useHistory } from "react-router";
 import useBreakpoint from "../../hooks/useBreakpoint";
 import { useTotal } from "../../hooks/useHomepage";
@@ -183,6 +184,219 @@ import biconomy from '../../assets/images/ecosystem/biconomy.png'
 import aw from '../../assets/images/ecosystem/AW.png'
 import daren from '../../assets/images/ecosystem/daren.svg'
 
+import * as PIXI from 'pixi.js';
+import { KawaseBlurFilter } from "@pixi/filter-kawase-blur";
+import SimplexNoise from "simplex-noise";
+import hsl from "hsl-to-hex";
+import debounce from "debounce";
+
+
+// return a random number within a range
+function random(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+// map a number from 1 range to another
+// @ts-ignore
+function map(n, start1, end1, start2, end2) {
+  return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
+}
+
+// Create a new simplex noise instance
+const simplex = new SimplexNoise();
+
+// ColorPalette class
+class ColorPalette {
+  constructor() {
+    this.setColors();
+  }
+
+   setColors() {
+          //~~random(180, 240);
+          this.hue = 180
+          this.complimentaryHue1 = this.hue + 30;
+          this.complimentaryHue2 = this.hue + 90;
+          // define a fixed saturation and lightness
+          this.saturation = 195;
+          this.lightness = 50;
+
+          // define a base color
+          this.baseColor = hsl(this.hue, this.saturation, this.lightness);
+          // define a complimentary color, 30 degress away from the base
+          this.complimentaryColor1 = hsl(
+            this.complimentaryHue1,
+            this.saturation,
+            this.lightness
+          );
+          // define a second complimentary color, 60 degrees away from the base
+          this.complimentaryColor2 = hsl(
+            this.complimentaryHue2,
+            this.saturation,
+            this.lightness
+          );
+
+          // store the color choices in an array so that a random one can be picked later
+          this.colorChoices = [
+            this.baseColor,
+            this.complimentaryColor1,
+            this.complimentaryColor2
+          ];
+        }
+
+  randomColor() {
+    // pick a random color
+    return this.colorChoices[~~random(0, this.colorChoices.length)].replace(
+      "#",
+      "0x"
+    );
+  }
+
+}
+
+// Orb class
+class Orb {
+  // Pixi takes hex colors as hexidecimal literals (0x rather than a string with '#')
+  constructor(fill = 0x040C0D) {
+    // bounds = the area an orb is "allowed" to move within
+    this.bounds = this.setBounds();
+    // initialise the orb's { x, y } values to a random point within it's bounds
+    this.x = random(this.bounds["x"].min, this.bounds["x"].max);
+    this.y = random(this.bounds["y"].min, this.bounds["y"].max);
+
+    // how large the orb is vs it's original radius (this will modulate over time)
+    this.scale = 1;
+
+    // what color is the orb?
+    this.fill = fill;
+
+    // the original radius of the orb, set relative to window height
+    this.radius = random(window.innerHeight / 3, window.innerHeight / 3);
+
+    // starting points in "time" for the noise/self similar random values
+    this.xOff = random(0, 1000);
+    this.yOff = random(0, 1000);
+    // how quickly the noise/self similar random values step through time
+    this.inc = 0.0008;
+
+    // PIXI.Graphics is used to draw 2d primitives (in this case a circle) to the canvas
+    this.graphics = new PIXI.Graphics();
+    this.graphics.alpha = 0.825;
+
+    // 250ms after the last window resize event, recalculate orb positions.
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        this.bounds = this.setBounds();
+      }, 250)
+    );
+  }
+
+  setBounds() {
+          // how far from the { x, y } origin can each orb move
+          const maxDist =
+            window.innerWidth < 10 ? window.innerWidth / 2 : window.innerWidth / 2.5;
+          // the { x, y } origin for each orb (the bottom right of the screen)
+          const originX = window.innerWidth / 2;
+          const originY =
+            window.innerWidth < 400 ?
+            window.innerHeight :
+            window.innerHeight / 2;
+
+          return {
+            x: {
+              min: originX - maxDist,
+              max: originX + maxDist
+            },
+            y: {
+              min: originY - maxDist,
+              max: originY + maxDist
+            }
+          };
+        }
+
+  update() {
+    // self similar "psuedo-random" or noise values at a given point in "time"
+    const xNoise = simplex.noise2D(this.xOff, this.xOff);
+    const yNoise = simplex.noise2D(this.yOff, this.yOff);
+    const scaleNoise = simplex.noise2D(this.xOff, this.yOff);
+
+    // map the xNoise/yNoise values (between -1 and 1) to a point within the orb's bounds
+    this.x = map(xNoise, -1, 1, this.bounds["x"].min, this.bounds["x"].max);
+    this.y = map(yNoise, -1, 1, this.bounds["y"].min, this.bounds["y"].max);
+    // map scaleNoise (between -1 and 1) to a scale value somewhere between half of the orb's original size, and 100% of it's original size
+    this.scale = map(scaleNoise, -1, 1, 0.5, 1);
+
+    // step through "time"
+    this.xOff += this.inc;
+    this.yOff += this.inc;
+  }
+
+  render() {
+    // update the PIXI.Graphics position and scale values
+    this.graphics.x = this.x;
+    this.graphics.y = this.y;
+    this.graphics.scale.set(this.scale);
+
+    // clear anything currently drawn to graphics
+    this.graphics.clear();
+
+    // tell graphics to fill any shapes drawn after this with the orb's fill color
+    this.graphics.beginFill(this.fill);
+    // draw a circle at { 0, 0 } with it's size set by this.radius
+    this.graphics.drawCircle(0, 0, this.radius);
+    // let graphics know we won't be filling in any more shapes
+    this.graphics.endFill();
+  }
+}
+
+let itl = setInterval(() => {
+  let box = document.getElementById('orb-canvas')
+  if (box) {
+    clearInterval(itl);
+    // Create PixiJS app
+    const app = new PIXI.Application({
+      // render to <canvas class="orb-canvas"></canvas>
+      view: box,
+      // auto adjust size to fit the current window
+      resizeTo: window,
+      // transparent background, we will be creating a gradient background later using CSS
+      transparent: true
+    });
+
+    app.stage.filters = [new KawaseBlurFilter(30, 10, true)];
+
+    // Create colour palette
+    const colorPalette = new ColorPalette();
+
+    // Create orbs
+    const orbs = [];
+
+    for (let i = 0; i < 10; i++) {
+      const orb = new Orb(colorPalette.randomColor());
+
+      app.stage.addChild(orb.graphics);
+
+      orbs.push(orb);
+    }
+
+    // Animate!
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      app.ticker.add(() => {
+        orbs.forEach((orb) => {
+          orb.update();
+          orb.render();
+        });
+      });
+    } else {
+      orbs.forEach((orb) => {
+        orb.update();
+        orb.render();
+      });
+    }
+  }
+}, 200)
+
+
 
 export const blinkAnimation = keyframes`
   0%, 100% {
@@ -285,15 +499,20 @@ export default function Homepage() {
     return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
   }
   // const simplex = new SimplexNoise();
+
+  useEffect(() => {
+
+  },[])
   
   return (
     <ContentWrapper>
       <Content1>
         <Head1>
-           <div className="blur-container">
+          <canvas id="orb-canvas" class="orb-canvas"></canvas>
+           {/* <div className="blur-container">
             <div className="shape" style={shapeStyle2}></div>
             <div className="shape" style={shapeStyle2}></div>
-          </div> 
+          </div>  */}
           {/* <div className="g-wrap">
             <div className="g-bubble g-bubble1"></div>
             <div className="g-bubble g-bubble2"></div>
